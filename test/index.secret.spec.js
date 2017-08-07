5fc2926a10e3dafb6f43f2fda69be323f90be5aa
Felix Heck <hi@whoTheHeck.de>
2017-08-07 14:01:01 +0200

    split tests

--- test/index.secret.spec.js
+++ test/index.secret.spec.js
@@ -9,13 +9,6 @@ test.afterEach.always('reset instances and prototypes', () => {
   nock.cleanAll()
 })
 
-test.cb.serial('throw error if plugin gets registered twice', (t) => {
-  helpers.getServer(undefined, (server) => {
-    t.throws(() => helpers.registerPlugin(server), Error)
-    t.end()
-  })
-})
-
 test.cb.serial('authentication does succeed', (t) => {
   helpers.mock(200, fixtures.content.userData)
 
@@ -125,78 +118,6 @@ test.cb.serial('authentication does fail – invalid header', (t) => {
       t.truthy(res)
       t.is(res.statusCode, 401)
       t.is(res.headers['www-authenticate'], 'Bearer error="Missing or invalid authorization header"')
-      t.end()
-    })
-  })
-})
-
-test.cb.serial('server method – authentication does succeed', (t) => {
-  helpers.mock(200, fixtures.content.userData)
-
-  helpers.getServer(undefined, (server) => {
-    server.kjwt.validate(`bearer ${fixtures.jwt.userData}`, (err, res) => {
-      t.falsy(err)
-      t.truthy(res)
-      t.truthy(res.credentials)
-      t.end()
-    })
-  })
-})
-
-test.cb.serial('server method – authentication does succeed – cache', (t) => {
-  helpers.mock(200, fixtures.content.userData)
-  helpers.mock(200, fixtures.content.userData)
-
-  helpers.getServer(undefined, (server) => {
-    server.kjwt.validate(`bearer ${fixtures.jwt.userData}`, () => {
-      server.kjwt.validate(`bearer ${fixtures.jwt.userData}`, (err, res) => {
-        t.falsy(err)
-        t.truthy(res)
-        t.truthy(res.credentials)
-        t.end()
-      })
-    })
-  })
-})
-
-test.cb.serial('server method – authentication does fail – invalid token', (t) => {
-  helpers.mock(200, { active: false })
-
-  helpers.getServer(undefined, (server) => {
-    server.kjwt.validate(`bearer ${fixtures.jwt.userData}`, (err, res) => {
-      t.falsy(res)
-      t.truthy(err)
-      t.truthy(err.isBoom)
-      t.is(err.output.statusCode, 401)
-      t.is(err.output.headers['WWW-Authenticate'], 'Bearer error="Invalid credentials"')
-      t.end()
-    })
-  })
-})
-
-test.cb.serial('server method – authentication does fail – invalid header', (t) => {
-  helpers.getServer(undefined, (server) => {
-    server.kjwt.validate(fixtures.jwt.userData, (err, res) => {
-      t.falsy(res)
-      t.truthy(err)
-      t.truthy(err.isBoom)
-      t.is(err.output.statusCode, 401)
-      t.is(err.output.headers['WWW-Authenticate'], 'Bearer error="Missing or invalid authorization header"')
-      t.end()
-    })
-  })
-})
-
-test.cb.serial('server method – authentication does fail – error', (t) => {
-  helpers.mock(400, 'an error', true)
-
-  helpers.getServer(undefined, (server) => {
-    server.kjwt.validate(`bearer ${fixtures.jwt.userData}`, (err, res) => {
-      t.falsy(res)
-      t.truthy(err)
-      t.truthy(err.isBoom)
-      t.is(err.output.statusCode, 401)
-      t.is(err.output.headers['WWW-Authenticate'], 'Bearer error="an error"')
       t.end()
     })
   })
