8933e1e53944d931ce00834ca6f2061513f8fb50
Felix Heck <hi@whoTheHeck.de>
2017-08-12 09:38:00 +0200

    refactor tests

--- test/index.live.spec.js
+++ test/index.live.spec.js
@@ -10,14 +10,19 @@ test.afterEach.always('reset instances and prototypes', () => {
 test.cb.serial('authentication does succeed', (t) => {
   helpers.mockIntrospect(200, fixtures.content.userData)
 
-  helpers.getServer(undefined, (server) => {
-    server.inject({
-      method: 'GET',
-      url: '/',
-      headers: {
-        authorization: `bearer ${fixtures.jwt.userData}`
-      }
-    }, (res) => {
+  const mockReq = {
+    method: 'GET',
+    url: '/',
+    headers: {
+      authorization: `bearer ${fixtures.jwt.userData}`
+    }
+  }
+
+  helpers.getServer(helpers.getOptions({
+    secret: fixtures.common.secret,
+    live: true
+  }), (server) => {
+    server.inject(mockReq, (res) => {
       t.truthy(res)
       t.is(res.statusCode, 200)
       t.end()
@@ -36,9 +41,11 @@ test.cb.serial('authentication does succeed – cached', (t) => {
     }
   }
 
-  helpers.getServer(helpers.getOptions({
+  helpers.getServer(helpers.getOptions(helpers.getOptions({
+    secret: fixtures.common.secret,
+    live: true,
     cache: true
-  }), (server) => {
+  })), (server) => {
     server.inject(mockReq, () => {
       server.inject(mockReq, (res) => {
         t.truthy(res)
@@ -52,14 +59,19 @@ test.cb.serial('authentication does succeed – cached', (t) => {
 test.cb.serial('authentication does success – valid roles', (t) => {
   helpers.mockIntrospect(200, fixtures.content.userData)
 
-  helpers.getServer(undefined, (server) => {
-    server.inject({
-      method: 'GET',
-      url: '/role',
-      headers: {
-        authorization: `bearer ${fixtures.jwt.userData}`
-      }
-    }, (res) => {
+  const mockReq = {
+    method: 'GET',
+    url: '/role',
+    headers: {
+      authorization: `bearer ${fixtures.jwt.userData}`
+    }
+  }
+
+  helpers.getServer(helpers.getOptions({
+    secret: fixtures.common.secret,
+    live: true
+  }), (server) => {
+    server.inject(mockReq, (res) => {
       t.truthy(res)
       t.is(res.statusCode, 200)
       t.end()
@@ -70,14 +82,19 @@ test.cb.serial('authentication does success – valid roles', (t) => {
 test.cb.serial('authentication does fail – invalid roles', (t) => {
   helpers.mockIntrospect(200, fixtures.content.userData)
 
-  helpers.getServer(undefined, (server) => {
-    server.inject({
-      method: 'GET',
-      url: '/role/guest',
-      headers: {
-        authorization: `bearer ${fixtures.jwt.userData}`
-      }
-    }, (res) => {
+  const mockReq = {
+    method: 'GET',
+    url: '/role/guest',
+    headers: {
+      authorization: `bearer ${fixtures.jwt.userData}`
+    }
+  }
+
+  helpers.getServer(helpers.getOptions({
+    secret: fixtures.common.secret,
+    live: true
+  }), (server) => {
+    server.inject(mockReq, (res) => {
       t.truthy(res)
       t.is(res.statusCode, 403)
       t.end()
@@ -88,14 +105,19 @@ test.cb.serial('authentication does fail – invalid roles', (t) => {
 test.cb.serial('authentication does fail – invalid token', (t) => {
   helpers.mockIntrospect(200, { active: false })
 
-  helpers.getServer(undefined, (server) => {
-    server.inject({
-      method: 'GET',
-      url: '/',
-      headers: {
-        authorization: `bearer ${fixtures.jwt.userData}`
-      }
-    }, (res) => {
+  const mockReq = {
+    method: 'GET',
+    url: '/',
+    headers: {
+      authorization: `bearer ${fixtures.jwt.userData}`
+    }
+  }
+
+  helpers.getServer(helpers.getOptions({
+    secret: fixtures.common.secret,
+    live: true
+  }), (server) => {
+    server.inject(mockReq, (res) => {
       t.truthy(res)
       t.is(res.statusCode, 401)
       t.is(res.headers['www-authenticate'], 'Bearer error="Invalid credentials"')
@@ -105,14 +127,19 @@ test.cb.serial('authentication does fail – invalid token', (t) => {
 })
 
 test.cb.serial('authentication does fail – invalid header', (t) => {
-  helpers.getServer(undefined, (server) => {
-    server.inject({
-      method: 'GET',
-      url: '/',
-      headers: {
-        authorization: fixtures.common.token
-      }
-    }, (res) => {
+  const mockReq = {
+    method: 'GET',
+    url: '/',
+    headers: {
+      authorization: fixtures.common.token
+    }
+  }
+
+  helpers.getServer(helpers.getOptions({
+    secret: fixtures.common.secret,
+    live: true
+  }), (server) => {
+    server.inject(mockReq, (res) => {
       t.truthy(res)
       t.is(res.statusCode, 401)
       t.is(res.headers['www-authenticate'], 'Bearer error="Missing or invalid authorization header"')
--- test/index.rpt.spec.js
+++ test/index.rpt.spec.js
@@ -12,16 +12,18 @@ test.afterEach.always('reset instances and prototypes', () => {
 test.cb.serial('authentication does succeed', (t) => {
   helpers.mockEntitlement(200, fixtures.content.userDataRpt)
 
+  const mockReq = {
+    method: 'GET',
+    url: '/',
+    headers: {
+      authorization: `bearer ${fixtures.jwt.userDataRpt}`
+    }
+  }
+
   helpers.getServer(helpers.getOptions({
     live: true
   }), (server) => {
-    server.inject({
-      method: 'GET',
-      url: '/',
-      headers: {
-        authorization: `bearer ${fixtures.jwt.userDataRpt}`
-      }
-    }, (res) => {
+    server.inject(mockReq, (res) => {
       t.truthy(res)
       t.is(res.statusCode, 200)
       t.deepEqual(JSON.parse(res.payload).sort(), targetScope)
@@ -59,16 +61,18 @@ test.cb.serial('authentication does succeed – cached', (t) => {
 test.cb.serial('authentication does success – valid roles', (t) => {
   helpers.mockEntitlement(200, fixtures.content.userDataRpt)
 
+  const mockReq = {
+    method: 'GET',
+    url: '/role',
+    headers: {
+      authorization: `bearer ${fixtures.jwt.userDataRpt}`
+    }
+  }
+
   helpers.getServer(helpers.getOptions({
     live: true
   }), (server) => {
-    server.inject({
-      method: 'GET',
-      url: '/role',
-      headers: {
-        authorization: `bearer ${fixtures.jwt.userDataRpt}`
-      }
-    }, (res) => {
+    server.inject(mockReq, (res) => {
       t.truthy(res)
       t.is(res.statusCode, 200)
       t.deepEqual(JSON.parse(res.payload).sort(), targetScope)
@@ -80,16 +84,18 @@ test.cb.serial('authentication does success – valid roles', (t) => {
 test.cb.serial('authentication does success – valid roles', (t) => {
   helpers.mockEntitlement(200, fixtures.content.userDataRpt)
 
+  const mockReq = {
+    method: 'GET',
+    url: '/role/rpt',
+    headers: {
+      authorization: `bearer ${fixtures.jwt.userDataRpt}`
+    }
+  }
+
   helpers.getServer(helpers.getOptions({
     live: true
   }), (server) => {
-    server.inject({
-      method: 'GET',
-      url: '/role/rpt',
-      headers: {
-        authorization: `bearer ${fixtures.jwt.userDataRpt}`
-      }
-    }, (res) => {
+    server.inject(mockReq, (res) => {
       t.truthy(res)
       t.is(res.statusCode, 200)
       t.deepEqual(JSON.parse(res.payload).sort(), targetScope)
@@ -101,16 +107,18 @@ test.cb.serial('authentication does success – valid roles', (t) => {
 test.cb.serial('authentication does fail – invalid roles', (t) => {
   helpers.mockEntitlement(200, fixtures.content.userDataRpt)
 
+  const mockReq = {
+    method: 'GET',
+    url: '/role/guest',
+    headers: {
+      authorization: `bearer ${fixtures.jwt.userDataRpt}`
+    }
+  }
+
   helpers.getServer(helpers.getOptions({
     live: true
   }), (server) => {
-    server.inject({
-      method: 'GET',
-      url: '/role/guest',
-      headers: {
-        authorization: `bearer ${fixtures.jwt.userDataRpt}`
-      }
-    }, (res) => {
+    server.inject(mockReq, (res) => {
       t.truthy(res)
       t.is(res.statusCode, 403)
       t.end()
@@ -121,16 +129,18 @@ test.cb.serial('authentication does fail – invalid roles', (t) => {
 test.cb.serial('authentication does fail – invalid token', (t) => {
   helpers.mockEntitlement(400, fixtures.content.userDataRpt)
 
+  const mockReq = {
+    method: 'GET',
+    url: '/',
+    headers: {
+      authorization: `bearer ${fixtures.jwt.userDataRpt}`
+    }
+  }
+
   helpers.getServer(helpers.getOptions({
     live: true
   }), (server) => {
-    server.inject({
-      method: 'GET',
-      url: '/',
-      headers: {
-        authorization: `bearer ${fixtures.jwt.userDataRpt}`
-      }
-    }, (res) => {
+    server.inject(mockReq, (res) => {
       t.truthy(res)
       t.is(res.statusCode, 401)
       t.is(res.headers['www-authenticate'], 'Bearer error="Invalid credentials"')
@@ -140,16 +150,18 @@ test.cb.serial('authentication does fail – invalid token', (t) => {
 })
 
 test.cb.serial('authentication does fail – invalid header', (t) => {
+  const mockReq = {
+    method: 'GET',
+    url: '/',
+    headers: {
+      authorization: fixtures.common.token
+    }
+  }
+
   helpers.getServer(helpers.getOptions({
     live: true
   }), (server) => {
-    server.inject({
-      method: 'GET',
-      url: '/',
-      headers: {
-        authorization: fixtures.common.token
-      }
-    }, (res) => {
+    server.inject(mockReq, (res) => {
       t.truthy(res)
       t.is(res.statusCode, 401)
       t.is(res.headers['www-authenticate'], 'Bearer error="Missing or invalid authorization header"')
