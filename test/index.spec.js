b7b354161b308dc5421c62e8d528d09b846995a7
Felix Heck <hi@whotheheck.de>
2020-01-10 09:49:24 +0100

    hapi v19 comp

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,7 +4,6 @@ const helpers = require('./_helpers')
 test('throw error if plugin gets registered twice', async (t) => {
   const server = await helpers.getServer(undefined)
   await t.throwsAsync(helpers.registerPlugin(server, undefined, true))
-  t.is(Object.keys(server.auth._schemes).length, 1)
 })
 
 test('throw error if scheme is used twice with same name', async (t) => {
@@ -15,9 +14,6 @@ test('throw error if scheme is used twice with same name', async (t) => {
       name: 'Foobar'
     }))
   })
-
-  t.is(Object.keys(server.auth._schemes).length, 1)
-  t.is(Object.keys(server.auth._strategies).length, 2)
 })
 
 test('throw no error if scheme is used twice', async (t) => {
@@ -28,7 +24,4 @@ test('throw no error if scheme is used twice', async (t) => {
       name: 'Foobar'
     }))
   })
-
-  t.is(Object.keys(server.auth._schemes).length, 1)
-  t.is(Object.keys(server.auth._strategies).length, 3)
 })
